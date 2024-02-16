cee15f27cdc885c9644fdfa596198b95b1cc4d49
tusharr1411 <tusharr1411@gmail.com>
2024-02-16 22:34:08 +0530

    tests updated

--- test/unit/NftMarketplace.test.js
+++ test/unit/NftMarketplace.test.js
@@ -15,46 +15,44 @@ const {assert, expact} = require("chai");
     
     
     beforeEach( async()=>{
-        console.log("0000000000")
         deployer = (await getNamedAccounts()).deployer;
         // player = (await getNamedAccounts()).player;
-
+        
         const accounts = await ethers.getSigners();
         player = accounts[1];
-
+        
         await deployments.fixture(["all"]);
-
+        
         nftMarketplace = await ethers.getContract("NftMarketPlace");// ethers.getContract grab whatever the 0th account i.e., deployer
+        console.log("0000000000")
         // nftMarketplace = await nftMarketplace.connect(player)// player is connected to NFT marketplace
         basicNft = await ethers.getContract("BasicNFT"); // connected to deployer
-
-
+        
+        
         await basicNft.mintNft()// minted by deployer
         //The approve function allows an NFT owner to give permission for a specific address to transfer their NFT token on their behalf.
         await basicNft.approve(nftMarketplace.address, TOKEN_ID)
 
-        // Test 1: It can be listed:
-        it("lists and can be bought", async()=>{
-            await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE);// deployer listed it
-            const playerConnectedToNftMakrketplace = nftMarketplace.connect(player);
-            await playerConnectedToNftMakrketplace.buyItem(basicNft.address, TOKEN_ID,{value: PRICE});
-
-            const newOwner = await basicNft.ownerOf(TOKEN_ID)// NFTs bult in function
-            const deployerProceeds = await nftMarketplace.getProceeds(deployer);
-
-            assert(newOwner.toString() == player.address);
-            assert(deployerProceeds.toString() == PRICE.toString());
-
-            // console.log("--------")
-            // console.log(deployer)
-            // console.log("--------")
-            // console.log(player)
-        })
-
-
-
+          
     })
+    
+    // Test 1: It can be listed:
+    it("lists and can be bought", async()=>{
+        await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE);// deployer listed it
+        const playerConnectedToNftMakrketplace = nftMarketplace.connect(player);
+        await playerConnectedToNftMakrketplace.buyItem(basicNft.address, TOKEN_ID,{value: PRICE});
 
+        const newOwner = await basicNft.ownerOf(TOKEN_ID)// NFTs bult in function
+        const deployerProceeds = await nftMarketplace.getProceeds(deployer);
+
+        assert(newOwner.toString() == player.address);
+        assert(deployerProceeds.toString() == PRICE.toString());
+
+        // console.log("--------")
+        // console.log(deployer)
+        // console.log("--------")
+        // console.log(player)
+    })
 
 
 
