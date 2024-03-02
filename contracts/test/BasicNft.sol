70dd6758aa39e0579dd6039f04453d6904107972
tusharr1411 <tusharr1411@gmail.com>
2024-03-02 09:22:56 +0530

    added parameter name for tokenURI function

--- contracts/test/BasicNft.sol
+++ contracts/test/BasicNft.sol
@@ -6,8 +6,7 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
-    string public constant TOKEN_URI =
-        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    string public constant TOKEN_URI = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     uint256 private s_tokenCounter;
 
@@ -21,7 +20,8 @@ contract BasicNFT is ERC721 {
         return s_tokenCounter;
     }
 
-    function tokenURI(uint256 /*tokenId*/) public view override returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        // require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         return TOKEN_URI;
     }
 
